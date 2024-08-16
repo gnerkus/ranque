10d82ac8eba6351631bd1461b716fd892593db94
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-08-16 13:57:45 +0200

    define an extension method to configure CORS

---
+++ streak/Extensions/ServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace streak.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            ArgumentNullException.ThrowIfNull(services);
+            
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+            });
+        }
+    }
+}
