6845c5ed8880813e1b56d47f72626a24f72d15ed
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-09-05 12:44:05 +0200

    scaffold leaderboard, org and participant entities

---
+++ Entities/Leaderboard.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities;
+
+public class Leaderboard
+{
+    [Column("LeaderboardId")]
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "Leaderboard name is a required field")]
+    [MaxLength(60, ErrorMessage = "Maximum length for the Name is 60 characters")]
+    public string? Name { get; set; }
+
+    [ForeignKey((nameof(Organization)))]
+    public Guid OrganizationId { get; set; }
+    public Organization? Organization { get; set; }
+}
---
+++ Entities/Participant.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities
+{
+    public class Participant
+    {
+        [Column("ParticipantId")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Participant name is a required field")]
+        [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Age is a required field")]
+        public int Age { get; set; }
+        
+        [Required(ErrorMessage = "Position is a required field")]
+        [MaxLength(20, ErrorMessage = "Maximum length for the Name is 20 characters")]
+        public string? Position { get; set; }
+        
+        [ForeignKey((nameof(Organization)))]
+        public Guid OrganizationId { get; set; }
+        public Organization? Organization { get; set; }
+    }
+}
+
+// TODO: establish relationship between leaderboard and participants
