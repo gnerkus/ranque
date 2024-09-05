f41abd92fc6427e775853d769dfe4fdab1943006
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-09-05 17:23:27 +0200

    define a context factory to enable creation of repository context from external project

--- Repository/RepositoryContext.cs
+++ Repository/RepositoryContext.cs
@@ -1,7 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Repository;
 
 public class RepositoryContext: DbContext
 {
+    public RepositoryContext(DbContextOptions options): base(options)
+    {
+        
+    }
+
+    public DbSet<Organization>? Organizations { get; set; }
+    public DbSet<Participant>? Participants { get; set; }
 }
