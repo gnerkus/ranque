8138bd25c8691e5a0610c0cc1d480e24da8308a8
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-10-01 20:29:00 +0200

    move participant endpoints from participants controller to org controller

--- Presentation/Controllers/OrganizationsController.cs
+++ Presentation/Controllers/OrganizationsController.cs
@@ -177,6 +177,94 @@ namespace Presentation.Controllers
             return NoContent();
         }
         
+        // ================ PARTICIPANTS =====================================
+        
+        [HttpGet("{orgId:guid}/participants")]
+        [HttpHead]
+        [ServiceFilter(typeof(ValidateMediaTypeAttribute))]
+        public async Task<IActionResult> GetParticipantsForOrganization(Guid orgId,
+            [FromQuery] ParticipantParameters parameters)
+        {
+            var linkParams = new LinkParameters(parameters, HttpContext);
+            
+            var pagedResult = await _service.ParticipantService.GetParticipantsAsync(orgId,
+                linkParams,
+                false);
+
+            Response.Headers.Add("X-Pagination", JsonSerializer.Serialize(pagedResult.metaData));
+
+            return pagedResult.linkResponse.HasLinks ? Ok(pagedResult.linkResponse
+            .LinkedEntities) : Ok(pagedResult.linkResponse.ShapedEntities);
+        }
+
+        [HttpGet("{orgId:guid}/participants/{id:guid}", Name = "GetParticipantForOrg")]
+        public async Task<IActionResult> GetParticipantForOrganization(Guid orgId, Guid id)
+        {
+            var participant = await _service.ParticipantService.GetParticipantAsync(orgId, id,
+                false);
+            return Ok(participant);
+        }
+
+        [HttpPost("{orgId:guid}/participants")]
+        public async Task<IActionResult> CreateParticipantForOrg(Guid orgId,
+            [FromBody] ParticipantForCreationDto pcptDto)
+        {
+            if (pcptDto is null) return BadRequest("Participant creation request body is null");
+
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
+
+            var participant = await _service.ParticipantService.CreateParticipantForOrgAsync(orgId,
+                pcptDto, false);
+
+            return CreatedAtRoute("GetParticipantForOrg", new { orgId, id = participant.Id },
+                participant);
+        }
+
+        [HttpPut("{orgId:guid}/participants/{participantId:guid}")]
+        public async Task<IActionResult> UpdateParticipantForOrg(Guid orgId, Guid participantId,
+            [FromBody] ParticipantForUpdateDto participantForUpdateDto)
+        {
+            if (participantForUpdateDto is null) return BadRequest("Participant body is null");
+
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
+
+            await _service.ParticipantService.UpdateParticipantForOrgAsync(orgId, participantId,
+                participantForUpdateDto, false, true);
+
+            return NoContent();
+        }
+
+        [HttpPatch("{orgId:guid}/participants/{id:guid}")]
+        public async Task<IActionResult> PartiallyUpdateParticipantForOrg(Guid orgId, Guid id,
+            [FromBody] JsonPatchDocument<ParticipantForUpdateDto> patchDoc)
+        {
+            if (patchDoc is null)
+                return BadRequest("patchDoc object sent from client is null.");
+            var result = await _service.ParticipantService.GetParticipantForPatchAsync(orgId, id,
+                false,
+                true);
+            patchDoc.ApplyTo(result.participantToPatch, ModelState);
+
+            TryValidateModel(result.participantToPatch);
+
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
+
+            await _service.ParticipantService.SaveChangesForPatchAsync(result.participantToPatch,
+                result.participant);
+            return NoContent();
+        }
+
+
+        [HttpDelete("{orgId:guid}/participants/{id:guid}")]
+        public async Task<IActionResult> DeleteParticipantForOrg(Guid orgId, Guid id)
+        {
+            await _service.ParticipantService.DeleteParticipantForOrgAsync(orgId, id, false);
+            return NoContent();
+        }
+        
         //================== GENERAL ========================================
 
         [HttpOptions]
