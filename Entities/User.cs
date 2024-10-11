24afc7c4efddb1c22a0525a4f95e3b2cf4fe26fb
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-10-11 17:10:44 +0200

    assign property defaults to User fields

--- Entities/User.cs
+++ Entities/User.cs
@@ -4,8 +4,8 @@ namespace Entities
 {
     public class User : IdentityUser
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
         public string? RefreshToken { get; set; }
         public DateTime RefreshTokenExpiryTime { get; set; }
     }
