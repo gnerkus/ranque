7923c8251ccde2907cd48707bc07519803f42647
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-10-11 17:21:20 +0200

    supress null reference warning

--- Presentation/ActionFilters/ValidationFilterAttribute.cs
+++ Presentation/ActionFilters/ValidationFilterAttribute.cs
@@ -11,7 +11,7 @@ namespace Presentation.ActionFilters
             var controller = context.RouteData.Values["controller"];
 
             var param = context.ActionArguments
-                .SingleOrDefault(x => x.Value.ToString().Contains("Dto")).Value;
+                .SingleOrDefault(x => x.Value != null && x.Value.ToString()!.Contains("Dto")).Value;
             if (param is null)
             {
                 context.Result = new BadRequestObjectResult(
