fd6de4afa3baa857c481cfaffc5b61e07533e54d
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-10-17 13:26:01 +0200

    reference entities project
    scaffold test for organization

---
+++ test/Core.Test/Organization.cs
@@ -0,0 +1,16 @@
+using Entities;
+
+namespace Core.Test;
+
+public class OrganizationTests
+{
+    [Fact]
+    public void Organization_ShouldHave_Name()
+    {
+        var org = new Organization
+        {
+            Name = "TestOrg"
+        };
+        Assert.NotNull(org.Name);
+    }
+}
