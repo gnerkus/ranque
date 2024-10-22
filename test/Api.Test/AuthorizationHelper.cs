0e1fcc3d6f855316e4065dfc7ad3be9448863a15
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-10-23 01:30:30 +0200

    add an app filter that ignores the previous authorization filter configured

---
+++ test/Api.Test/AuthorizationHelper.cs
@@ -0,0 +1,65 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace Entities.Test
+{
+    internal class AutoAuthorizeStartupFilter : IStartupFilter
+    {
+        public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
+        {
+            return builder =>
+            {
+                builder.UseMiddleware<AutoAuthorizeMiddleware>();
+                next(builder);
+            };
+        }
+    }
+    internal class AutoAuthorizeMiddleware
+    {
+        private readonly RequestDelegate _rd;
+        public AutoAuthorizeMiddleware(RequestDelegate rd)
+        {
+            _rd = rd;
+        }
+        
+        public async Task Invoke(HttpContext httpContext)
+        {
+            var identity = new ClaimsIdentity("Bearer");
+
+            identity.AddClaim(new Claim("sub", "1234567"));
+            identity.AddClaim(new Claim(ClaimTypes.Name, "test-name"));
+            
+            identity.AddClaims(GetClaimsBasedOnHttpHeaders(httpContext));
+
+            httpContext.User.AddIdentity(identity);
+            await _rd.Invoke(httpContext);
+        }
+        
+        /// <summary>
+        /// add claims to the identity based on the headers in the request
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        private static List<Claim> GetClaimsBasedOnHttpHeaders(HttpContext context)
+        {
+            const string headerPrefix = "X-Test-";
+
+            var claims = new List<Claim>();
+
+            var claimHeaders = context.Request.Headers.Keys.Where(k => k.StartsWith(headerPrefix));
+            foreach (var header in claimHeaders)
+            {
+                var value = context.Request.Headers[header];
+                var claimType = header[headerPrefix.Length..];
+                if (!string.IsNullOrEmpty(value))
+                {
+                    claims.Add(new Claim(claimType == "role" ? ClaimTypes.Role : claimType, value!));
+                }
+            }
+
+            return claims;
+        }
+    }
+}
