92e3b38b1bcb3cb6418dcc3c3e8dbcd2d5379cc7
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-10-22 23:10:38 +0200

    add extension method to deserialize results

---
+++ test/Api.Test/HttpClientExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+
+namespace Entities.Test
+{
+    public static class HttpClientExtensions
+    {
+        private static readonly JsonSerializerOptions Options = new ()
+        {
+            IncludeFields = true,
+        };
+        public static async Task<T> GetAndDeserialize<T>(this HttpClient client, 
+        HttpRequestMessage 
+        message)
+        {
+            var response = await client.SendAsync(message);
+            var result = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<T>(result, Options)!;
+        }
+    }
+}
