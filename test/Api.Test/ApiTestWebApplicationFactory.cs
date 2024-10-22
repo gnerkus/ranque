fda306f4c9968857807842881eeb094f24d0c970
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-10-22 16:15:35 +0200

    build test app factory using Testingcontainers

--- test/Api.Test/ApiTestWebApplicationFactory.cs
+++ test/Api.Test/ApiTestWebApplicationFactory.cs
@@ -1,50 +1,66 @@
-﻿using System.Data.Common;
-using Contracts;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.AspNetCore.TestHost;
-using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Repository;
+using Testcontainers.MsSql;
 
 namespace Entities.Test;
     
-public class ApiTestWebApplicationFactory<TProgram>: WebApplicationFactory<TProgram> where 
-TProgram: class
+public class ApiTestWebApplicationFactory: WebApplicationFactory<Program>, IAsyncLifetime
 {
+    private RepositoryContext _dbContext = default!;
+    private string MsSqlConnectionString => _dbContainer.GetConnectionString();
+    private readonly MsSqlContainer _dbContainer = new MsSqlBuilder().Build();
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.ConfigureServices(services =>
         {
-            // Find the existing dbcontext and remove it
+            // Find the existing db context and remove it
             var dbContextDescriptor = services.SingleOrDefault(d => d.ServiceType == typeof
                 (DbContextOptions<RepositoryContext>));
 
-            services.Remove(dbContextDescriptor);
-
-            var dbConnectionDescriptor = services.SingleOrDefault(d => d.ServiceType == typeof
-                (DbConnection));
-
-            services.Remove(dbConnectionDescriptor);
-            
-            // Create open SqliteConnection so EF won't automatically close it.
-            services.AddSingleton<DbConnection>(container =>
+            if (dbContextDescriptor is not null)
             {
-                var connection = new SqliteConnection("DataSource=:memory:");
-                connection.Open();
-
-                return connection;
-            });
+                services.Remove(dbContextDescriptor);
+            }
 
             // Use SQLite as in-memory database for the tests
             services.AddDbContext<RepositoryContext>((container, options) =>
             {
-                var connection = container.GetRequiredService<DbConnection>();
-                options.UseSqlite(connection);
+                options.UseSqlServer(MsSqlConnectionString);
             });
         });
 
         builder.UseEnvironment("Development");
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _dbContainer.StartAsync();
+
+        var dbContextOptions = new DbContextOptionsBuilder<RepositoryContext>()
+            .UseSqlServer(MsSqlConnectionString, b =>
+            {
+                b.EnableRetryOnFailure();
+                b.MigrationsAssembly("Core");
+            })
+            .Options;
+        _dbContext = new RepositoryContext(dbContextOptions);
+        await _dbContext.Database.MigrateAsync();
+    }
+
+    public new async Task DisposeAsync()
+    {
+        if (_dbContext is not null)
+        {
+            await _dbContext.DisposeAsync();
+        }
+
+        if (_dbContainer is not null)
+        {
+            await _dbContainer.StopAsync();
+            await _dbContainer.DisposeAsync();
+        }
     }
 }
