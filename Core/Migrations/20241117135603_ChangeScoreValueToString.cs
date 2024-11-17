72ac99790d87bd7840841ba28356ab5c851326e3
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-11-17 15:09:05 +0100

    generate migrations for update to Score entity

---
+++ Core/Migrations/20241117135603_ChangeScoreValueToString.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace streak.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeScoreValueToString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Leaderboards_Organizations_OrganizationId",
+                table: "Leaderboards");
+
+            migrationBuilder.DropColumn(
+                name: "Value",
+                table: "Scores");
+
+            migrationBuilder.AddColumn<string>(
+                name: "JsonValue",
+                table: "Scores",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Organizations",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Leaderboards",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Leaderboards_Organizations_OrganizationId",
+                table: "Leaderboards",
+                column: "OrganizationId",
+                principalTable: "Organizations",
+                principalColumn: "OrganizationId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Leaderboards_Organizations_OrganizationId",
+                table: "Leaderboards");
+
+            migrationBuilder.DropColumn(
+                name: "JsonValue",
+                table: "Scores");
+
+            migrationBuilder.AddColumn<float>(
+                name: "Value",
+                table: "Scores",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Organizations",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Leaderboards",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Leaderboards_Organizations_OrganizationId",
+                table: "Leaderboards",
+                column: "OrganizationId",
+                principalTable: "Organizations",
+                principalColumn: "OrganizationId");
+        }
+    }
+}
