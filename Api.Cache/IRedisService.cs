5e933a82aec021b4d80f1039f6209d74307b4111
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-12-06 13:22:02 +0100

    scaffold Redis service

---
+++ Api.Cache/IRedisService.cs
@@ -0,0 +1,8 @@
+﻿namespace Api.Cache
+{
+    public interface IRedisService
+    {
+        public T GetCachedData<T>(string key);
+        public void SetCachedData<T>(string key, T data, TimeSpan cacheDuration);
+    }
+}
---
+++ Api.Cache/RedisCacheService.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Api.Cache;
+
+public class RedisCacheService: IRedisService
+{
+    private readonly IDistributedCache _cache;
+
+    public RedisCacheService(IDistributedCache cache)
+    {
+        _cache = cache;
+    }
+    
+    public T GetCachedData<T>(string key)
+    {
+        var jsonData = _cache.GetString(key);
+
+        if (jsonData == null)
+            return default(T);
+
+        return JsonSerializer.Deserialize<T>(jsonData);
+    }
+    
+    public void SetCachedData<T>(string key, T data, TimeSpan cacheDuration)
+    {
+        var options = new DistributedCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = cacheDuration
+        };
+
+        var jsonData = JsonSerializer.Serialize(data);
+        _cache.SetString(key, jsonData, options);
+    }
+}
