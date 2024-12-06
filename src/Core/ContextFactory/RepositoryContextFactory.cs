c176aecc7256511a451ae5b6bc13874a4f081ad9
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-12-06 11:27:50 +0100

    read connection string from config

--- src/Core/ContextFactory/RepositoryContextFactory.cs
+++ src/Core/ContextFactory/RepositoryContextFactory.cs
@@ -9,15 +9,12 @@ namespace streak.ContextFactory
         public RepositoryContext CreateDbContext(string[] args)
         {
             var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
- 
             var configuration = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json")
                 .AddJsonFile($"appsettings.{environment}.json")
                 .Build();
-
-            var connectionString = configuration.GetConnectionString
-                    ("AZURE_SQL_CONNECTIONSTRING");
+            var connectionString = configuration["AZURE_SQL_CONNECTIONSTRING"];
 
             var builder = new DbContextOptionsBuilder<RepositoryContext>()
                 .UseSqlServer(
